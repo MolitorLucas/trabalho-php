4ed3b05016483a1f3d84b2506e19bd70b6a29030
TNDNS <103520454+TNDNS@users.noreply.github.com>
2023-06-27 17:35:23 -0300

    Criacao da Base de Telas/Cruds

--- VIEW/DESENVOLVEDORA/insDesenvolvedora.php
+++ VIEW/DESENVOLVEDORA/insDesenvolvedora.php
@@ -10,28 +10,86 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+
 </head>
 
 <body>
+<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="lstDesenvolvedora.php"><link href=""><img src="../../img/logoFinal.png"></link></a>
+        </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link  ml-10" href="#">USUÁRIOS</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link  ml-10" href="#">PUBLISHERS</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link  ml-10" href="#">JOGOS</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link  ml-10" href="#">GÊNEROS</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+        </nav>
 
+<div class="pagina">
+    <div class="containerGeral">
+                <div class="titulo">
+                    <h1>INSERIR</h1>
+                </div>
+                <div class='containerDev'>
+                    <form>
+                    <div class="mb-3">
+                        <label for="nomeInput" class="textoInput form-label">Nome</label>
+                        <input type="nome" class="input form-control" id="nomeInput" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="oritemInput" class="textoInput form-label">Origem</label>
+                        <input type="origem" class="input form-control" id="oritemImput">
+                    </div>
+                    <div class="form-check mb-3 ml-3 form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                    <label class="check form-check-label" for="flexSwitchCheckDefault">Sua desenvolvedora é Indie?</label>
+                    </div>
 
-    <div class='containerDev'>
-    <form>
-    <div class="mb-3">
-        <label for="nomeInput" class="textoInput form-label">Nome</label>
-        <input type="nome" class="input form-control" id="nomeInput" aria-describedby="emailHelp">
+                    <div class="posBotoes">
+                        <button type="submit" class="botaoConfirmar btn btn-primary">
+                            <iconify-icon icon="memory:sword" style="color: white;" width="30" height="30"></iconify-icon>
+                        </button>
+                        <button type="submit" class="botaoRejeitar btn btn-primary">
+                            <iconify-icon icon="memory:remove-circle" style="color: black;" width="30" height="30"></iconify-icon>
+                        </button>
+                    </form>
+                </div>
     </div>
-    <div class="mb-3">
-        <label for="oritemInput" class="textoInput form-label">Origem</label>
-        <input type="origem" class="input form-control" id="oritemImput">
-    </div>
-    <div class="form-check mb-3 ml-3 form-switch">
-    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-    <label class="check form-check-label" for="flexSwitchCheckDefault">Sua desenvolvedora é Indie?</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    </div>
+
+    
+        
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</div>
+
+    <div class="footer">
+        <p class="lacottickDir"><a class="tituloTop" href="https://www.flowcode.com/page/lacottickrpg?fbclid=PAAaZ0sE1Wxso9pBKdSCVOMncJv-n_1JD733tAprU2VIe319u5GKpKnirurL0">© [2023] Lacottick.</a> 
+        <br>Todos os direitos reservados. O conteúdo deste site, incluindo textos, imagens, gráficos e outros materiais, está 
+        protegido por leis de direitos autorais. É proibida a reprodução, distribuição, exibição ou transmissão de qualquer parte 
+        ou do todo do conteúdo sem autorização prévia por escrito da Lacottick. Todas as marcas registradas, logotipos e marcas de 
+        serviço exibidos são propriedade exclusiva da Lacottick, e qualquer uso não autorizado é estritamente proibido. Reservamo-nos 
+        o direito de tomar medidas legais para proteger nossos direitos autorais e outras propriedades intelectuais, incluindo a busca 
+        de indenizações financeiras e ações judiciais, se necessário. Agradecemos sua compreensão e respeito pelos direitos autorais 
+        e propriedade intelectual da Lacottick.
+        </p>   
+        <p class="lacottickBy">By <a class="tituloTop" href="https://www.linkedin.com/in/nicolasppenga/">Nicolas Petruci</a> & <a class="tituloTop" href="https://www.linkedin.com/in/lucas-molitor-1b1931230/">Lucas Roberto</a></p>
+    </div>
+</div>
 </body>
 </html>
