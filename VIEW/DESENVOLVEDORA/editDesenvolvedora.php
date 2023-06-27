b9048033f22fbf90b9c1f35f2b1f6d708c189707
Nicolas Petruci Penga <103520454+TNDNS@users.noreply.github.com>
2023-06-27 19:48:43 -0300

    subindo coisas pro lucas

---
+++ VIEW/DESENVOLVEDORA/editDesenvolvedora.php
@@ -0,0 +1,79 @@
+<?php
+include_once 'C:\xampp\htdocs\trabalho-php\BLL\blldesenvolvedora.php';
+$id = $_GET['id'];
+
+$bll = new  \BLL\bllDesenvolvedora();
+$desenvolvedora = $bll->selectId($id);
+
+echo $desenvolvedora->getNome();
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+
+    <link href="../css/desenvolvedora.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+
+</head>
+
+<body>
+<?php include_once '../navbar.php';?>
+
+<div class="pagina">
+    <div class="containerGeral">
+                <div class="titulo">
+                    <h1>EDITAR</h1>
+                </div>
+                <div class='containerDev'>
+                    <form method="POST" action="recinsDesenvolvedor.php" id="formInsere">
+                    <div class="mb-3">
+                        <label for="idInput" class="textoInput form-label">Nome Da Desenvolvedora</label>
+                        <input type="id" class="input form-control" id="idInput" name="idInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="nomeInput" class="textoInput form-label">Nome Da Desenvolvedora</label>
+                        <input type="nome" class="input form-control" id="nomeInput" name="nomeInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="origemInput" class="textoInput form-label">País de Origem</label>
+                        <input type="origem" class="input form-control" id="origemInput" name="origemInput">
+                    </div>
+                    <div class="form-check mb-3 ml-3 form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="indieCheckBox" name="indieCheckBox">
+                    <label class="check form-check-label" for="indieCheckBox">Sua desenvolvedora é Indie?</label>
+                    </div>
+
+                    <div class="posBotoes">
+                        <button type="submit" class="botaoConfirmar btn btn-primary">
+                            <iconify-icon icon="memory:sword" style="color: white;" width="30" height="30"></iconify-icon>
+                        </button>
+                        <button type="reset" class="botaoRejeitar btn btn-primary">
+                            <iconify-icon icon="memory:remove-circle" style="color: black;" width="30" height="30"></iconify-icon>
+                        </button>
+                    </form>
+                </div>
+    </div>
+
+    
+        
+    
+
+</div>
+
+<?php include_once '../footer.php';?>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ VIEW/footer.php
@@ -0,0 +1,15 @@
+
+
+<div class="footer">
+        <p class="lacottickDir"><a class="tituloTop" href="https://www.flowcode.com/page/lacottickrpg?fbclid=PAAaZ0sE1Wxso9pBKdSCVOMncJv-n_1JD733tAprU2VIe319u5GKpKnirurL0">© [2023] Lacottick.</a> 
+        <br>Todos os direitos reservados. O conteúdo deste site, incluindo textos, imagens, gráficos e outros materiais, está 
+        protegido por leis de direitos autorais. É proibida a reprodução, distribuição, exibição ou transmissão de qualquer parte 
+        ou do todo do conteúdo sem autorização prévia por escrito da Lacottick. Todas as marcas registradas, logotipos e marcas de 
+        serviço exibidos são propriedade exclusiva da Lacottick, e qualquer uso não autorizado é estritamente proibido. Reservamo-nos 
+        o direito de tomar medidas legais para proteger nossos direitos autorais e outras propriedades intelectuais, incluindo a busca 
+        de indenizações financeiras e ações judiciais, se necessário. Agradecemos sua compreensão e respeito pelos direitos autorais 
+        e propriedade intelectual da Lacottick.
+        </p>   
+        <p class="lacottickBy">By <a class="tituloTop" href="https://www.linkedin.com/in/nicolasppenga/">Nicolas Petruci</a> & <a class="tituloTop" href="https://www.linkedin.com/in/lucas-molitor-1b1931230/">Lucas Roberto</a><br><br></p>
+    
+    </div>
