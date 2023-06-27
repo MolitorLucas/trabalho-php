15eb44499d9d92206b120b1fede3ecfd0d46b51d
MolitorLucas <lucas.lacottick@gmail.com>
2023-06-27 20:45:08 -0300

    update desenvolvedora

--- BLL/bllDesenvolvedora.php
+++ BLL/bllDesenvolvedora.php
@@ -1,53 +1,68 @@
-<?php 
-    namespace BLL;
+<?php
 
-    use \DAL\DalDesenvolvedora;
+namespace BLL;
 
-    include_once 'C:\xampp\htdocs\trabalho-php\DAL\dalDesenvolvedora.php';
+use \DAL\DalDesenvolvedora;
 
-    class BllDesenvolvedora {
-        private DalDesenvolvedora $dal;
+include_once 'C:\xampp\htdocs\trabalho-php\DAL\dalDesenvolvedora.php';
 
-        public function __construct(){
-            $this->dal = new DalDesenvolvedora();
+class BllDesenvolvedora
+{
+    private DalDesenvolvedora $dal;
+
+    public function __construct()
+    {
+        $this->dal = new DalDesenvolvedora();
+    }
+    public function select()
+    {
+
+        try {
+            $desenvolvedoras = $this->dal->select();
+            return $desenvolvedoras;
+        } catch (\Exception $e) {
+            echo ($e->getMessage());
         }
-        public function select(){
+    }
 
+    public function selectId(int $id)
+    {
+        try {
+            $desenvolvedora = $this->dal->selectID($id);
+            return $desenvolvedora;
+        } catch (\Exception $e) {
+            echo ($e->getMessage());
+        }
+    }
+
+    public function insert(\MODEL\Desenvolvedora $desenvolvedora)
+    {
+        if ($desenvolvedora->getNome() != "" && $desenvolvedora->getOrigem() != "") {
             try {
-                $desenvolvedoras = $this->dal->select();
-                return $desenvolvedoras;
-            }
-            catch(\Exception $e) {
-                echo($e->getMessage());
-            }
-        }
-
-        public function selectId(int $id)
-        {
-            try {
-                $desenvolvedora = $this->dal->selectID($id);
-                return $desenvolvedora;
-            }
-            catch(\Exception $e){
-                echo($e->getMessage());
-            }
-        }
-
-        public function insert(\MODEL\Desenvolvedora $desenvolvedora){
-            if($desenvolvedora->getNome() != "" && $desenvolvedora->getOrigem()!=""){
-                try {
-                    $this->dal->insert($desenvolvedora);
-                } catch (\Exception $e) {
-                    echo ($e->getMessage());
-                }
-            }
-        }
-
-        public function delete(int $id){
-            try{
-                $this->dal->delete($id);
+                $this->dal->insert($desenvolvedora);
             } catch (\Exception $e) {
                 echo ($e->getMessage());
             }
         }
     }
+
+    public function delete(int $id)
+    {
+        try {
+            $this->dal->delete($id);
+        } catch (\Exception $e) {
+            echo ($e->getMessage());
+        }
+    }
+
+    public function update(\MODEL\Desenvolvedora $desenvolvedora)
+    {
+        if ($desenvolvedora->getNome() != "" && $desenvolvedora->getOrigem() != "") {
+            try {
+                $this->dal->update($desenvolvedora);
+            } catch (\Exception $e) {
+                echo ($e->getMessage());
+            }
+        }
+    }
+}
