c2b3a8e892837ccfecc5aedd4093a7328ec0a196
Nicolas Petruci Penga <103520454+TNDNS@users.noreply.github.com>
2023-06-27 22:39:53 -0300

    Lista Atualizada

--- VIEW/DESENVOLVEDORA/lstDesenvolvedora.php
+++ VIEW/DESENVOLVEDORA/lstDesenvolvedora.php
@@ -17,13 +17,51 @@ $lstDesenvolvedora = $bll->select();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link href="../css/insert.css" rel="stylesheet">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
     <title>Listar desenvolvedoras</title>
+
+    <style>
+        .containerDev{
+            background-color: white !important;
+            width: 75%;
+            height: 1000px !important;
+            padding: 5.5%;
+            margin-left: auto;
+            margin-right: auto;
+            border-radius: 20px 20px 20px 20px !important;
+            box-shadow: 0px 0px #f2f68d !important;
+        }
+        .botao{
+        background-color: blueviolet !important;
+        border-Radius: 10px 10px 0px 0px;
+        width: 100%;
+        align-items: center;
+        justify-items: center;
+        font-weight: 700;
+        font-size: 16px;
+    }
+
+        
+        
+        
+    </style>
+
 </head>
 
 <body>
-    <div class="container">
+
+<?php include_once '../navbar.php'; ?>
+
+<div class='pagina'>
+    <div class="containerGeral">
+        
+    <div class='containerDev'>
+
+        <button class='botao btn btn-sm btn-primary' onclick="JavaScript:location.href='insDesenvolvedora.php'">Inserir Desenvolvedora</button>
+
         <p class="h1 py-2 bg-warning font-monospace text-center">Listar Desenvolvedoras</p>
         <table class="table table-dark table-stripped table-hover">
             <thead>
@@ -59,6 +97,7 @@ $lstDesenvolvedora = $bll->select();
             </tbody>
 
         </table>
+            </div>
     </div>
     <script>
     function remover(id) {
@@ -86,9 +125,11 @@ $lstDesenvolvedora = $bll->select();
         </div>
     </div>
     </div>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+    <?php include_once '../footer.php'; ?>
 </body>
 
 </html>
