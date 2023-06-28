d8c18dbdcd28e4cd7d0ed90067fe39bc2421a3ea
Nicolas Petruci Penga <103520454+TNDNS@users.noreply.github.com>
2023-06-28 11:09:25 -0300

    Correção do Login

--- VIEW/index.php
+++ VIEW/index.php
@@ -11,6 +11,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
     
+    
+
     <style>
         .container-login,
         .box-login {
@@ -20,12 +22,12 @@
             align-items: center;
             flex-direction: row;
             width: 100vw;
-            height: 100vh;
+            height: 650px;
         }
 
         .box-login {
             width: 300px;
-            height: 300px;
+            height: 375px;
             flex-direction: column;
             border-radius: 25px;
         }
@@ -35,22 +37,38 @@
         }
 
         button {
-            border-left: 12.5px solid #DB00FF !important;
-            border-radius: 50px !important;
+            margin-right: 35px !important;
+            margin-left: auto !important;
         }
 
         nav {
             height: 75px;
         }
+
+        .user{
+            border: 5px solid white;
+            border-radius: 10px;
+        }
     </style>
+
+    <link href="css/insert.css" rel="stylesheet">
     <title>Login</title>
 </head>
 
 <body>
-    <nav class="navbar text-white navbar-dark bg-dark">
-    </nav>
+<?php include_once 'navbar3.php'; ?>
+<div class='pagina'>
+        <div class="containerGeral">
+
+            
+
+    <link href="../css/insert.css" rel="stylesheet">
     <div class="container-login text-center">
-        <div class="box-login bg-dark">
+        <div class="box-login row-3 bg-dark ">
+        <br>
+        <div class='user'>
+            <iconify-icon icon="memory:account" style="color: white;" width="100" height="100"></iconify-icon>
+        </div>
             <form method="POST" action="login.php">
                 <div class="usuario my-2">
                     <div class="">
@@ -68,15 +86,19 @@
                         <input id="password" name="senha" type="password" class="form-control font-monospace" />
                     </div>
                 </div>
-                <div class="botao my-2">
-                    <button type="submit" name="action" class="btn btn-warning text-white"><iconify-icon width="24" icon="memory:play"></iconify-icon></button>
+                <div class="posBotoes">
+                    <button type="submit" class="botaoConfirmar btn btn-primary"><iconify-icon icon="memory:play" style="color: white;" width="30" height="30"></iconify-icon></button>
                 </div>
             </form>
         </div>
     </div>
-
+    </div>
+    </div>
+   
+    
+    <?php include_once 'footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    
 </body>
 
 </html>
